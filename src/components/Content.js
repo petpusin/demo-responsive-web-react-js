c7b7a6b7f01f879dcb20a6557d1e835c53347dba
petpusin <pet_ray@windowslive.com>
2022-09-20 15:28:57 +0700

    implement content component and css

---
+++ src/components/Content.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../css/Content.css'
+function Content() {
+    return (
+        <div className='container'>
+            <section className='content-con'>
+                <div className='content-l'>
+                    <img src='https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='' />
+                </div>
+                <div className='content-r'>
+                    <h2>Some Title</h2>
+                    <p>Laboris sit ea reprehenderit fugiat aute cupidatat incididunt anim ex et sunt laboris. Aute laborum commodo incididunt pariatur ullamco ad voluptate amet eu cupidatat pariatur cillum. Non ipsum anim ad fugiat voluptate elit officia.</p>
+                </div>
+            </section>
+            <section className='content-con'>
+                <div className='content-r'>
+                    <h2>Some Title</h2>
+                    <p>Laboris sit ea reprehenderit fugiat aute cupidatat incididunt anim ex et sunt laboris. Aute laborum commodo incididunt pariatur ullamco ad voluptate amet eu cupidatat pariatur cillum. Non ipsum anim ad fugiat voluptate elit officia.</p>
+                </div>
+                <div className='content-l'>
+                    <img src='https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='' />
+                </div>
+            </section>
+            <section className='content-con'>
+                <div className='content-l'>
+                    <img src='https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='' />
+                </div>
+                <div className='content-r'>
+                    <h2>Some Title</h2>
+                    <p>Laboris sit ea reprehenderit fugiat aute cupidatat incididunt anim ex et sunt laboris. Aute laborum commodo incididunt pariatur ullamco ad voluptate amet eu cupidatat pariatur cillum. Non ipsum anim ad fugiat voluptate elit officia.</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Content
