6f0835f32851d85b54c540820a28c35cd949bb4e
petpusin <pet_ray@windowslive.com>
2022-09-20 15:29:26 +0700

    implement footer component and css

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/Footer.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <p>Copyright 2021. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
